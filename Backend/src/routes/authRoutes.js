efa68d0ee881f0303a2c404203169ce81cdc1431
Nikhil Tedla <iamnikhil2612@gmail.com>
2025-05-12 11:37:23 +0530

    Revert "first commit"
    This reverts commit a593312d730c0ff4e4a1311b30a54d08ef353e1c.

--- Backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const { protect } = require('../Middleware/authMiddleware');
-const { getuserProfile, registerUser, loginUser, updateProfile } = require('../controllers/authControllers');
-const router=express.Router();
-router.post("/register",registerUser)
-.post("/login",loginUser)
-.get("/profile",protect,getuserProfile)
-.put("/profile",protect,updateProfile);
-module.exports=router;
